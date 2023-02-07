c09444e9c2f62d8d1809dfe3969d53bda8b11075
Alex Clayton <alexander.clayton.email@gmail.com>
2023-02-07 13:57:40 -0600

    updated src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { StaleWhileRevalidate } = require('workbox-strategies');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -24,7 +25,19 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
+precacheAndRoute(self.__WB_MANIFEST);
+
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// TODO: Implement asset caching Not sure if this is correct...
+registerRoute(
+    ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+    new StaleWhileRevalidate({
+        cacheName: 'asset-cache',
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+        ],
+    }),
+);
